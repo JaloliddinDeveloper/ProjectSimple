ef5cce8bf7ab66a05a42b987adb8380878aec9fa
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-03-16 20:34:00 +0500

    ProjectSimple Initialize

---
+++ ProjectSimple.Api/Brokers/Storages/IStorageBroker.Product.cs
@@ -0,0 +1,18 @@
+﻿//==================================================
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use To Find Comfort And Peace
+//==================================================
+
+using ProjectSimple.Api.Models.Foundations.Products;
+
+namespace ProjectSimple.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Product> InsertProductAsync(Product product);
+       // IQueryable<Product> SelectAllProducts();
+       // ValueTask<Product> SelectProductByIdAsync(Guid id);
+       // ValueTask<Product> UpdateProductAsync(Product product);
+       // ValueTask<Product> DeleteProductAsync(Product product);
+    }
+}
---
+++ ProjectSimple.Api/Models/Foundations/Products/Exceptions/AlreadyExistProductException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------
+// Copyright (c) Coalition of Good-hearted Engineers
+// Free to use to find comfort and pease
+// ---------------------------------------------------
+
+using Xeptions;
+
+namespace ProjectSimple.Api.Models.Foundations.Products.Exceptions
+{
+    public class AlreadyExistProductException: Xeption
+    {
+        public AlreadyExistProductException(Exception innerException)
+            :base(message:"Guest already exist ",innerException)
+        { }
+    }
+}
